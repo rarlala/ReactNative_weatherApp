6cc60c134d955b1cdf38fd153f4eafbab768853d
JEONGSUJI <wtw9611@gmail.com>
2020-01-07 03:49:07 +0900

    react native 작동 방식 알아보기

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello!!</Text>
+      <Text style={styles.text}>Hello!!</Text>
     </View>
   );
 }
@@ -12,8 +12,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'orange',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text : {
+      color: "white"
+  }
 });
