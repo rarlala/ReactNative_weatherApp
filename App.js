5509103931435f194ef0c72f7af428133d274bfc
JEONGSUJI <wtw9611@gmail.com>
2020-01-12 21:20:36 +0900

    Weather.js 추가 및 temp 가져오기

--- App.js
+++ App.js
@@ -1,19 +1,27 @@
 import React from 'react';
+import { Alert } from 'react-native';
 import Loading from "./Loading";
 import * as Location from "expo-location";
-import { Alert } from 'react-native';
+import axios from "axios";
+import Weather from './Weather';
+
+const API_KEY = "f3beb5f6b388254461b97dcf7af70f12";
 
 export default class extends React.Component {
   state = {
     isLoading: true
   };
 
+  getWeather = async(latitude, longitude) => {
+    const { data } = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`);
+    this.setState({isLoading:false, temp: data.main.temp});
+  }
   getLocation = async() => {
     try{
       await Location.requestPermissionsAsync();
       const {coords : {latitude, longitude}} = await Location.getCurrentPositionAsync();
       // Send to API and get weather
-      this.setState({isLoading: false});
+      this.getWeather(latitude, longitude);
     } catch(error) {
       Alert.alert("Can't find you","So sad");
     }
@@ -24,7 +32,7 @@ export default class extends React.Component {
   };
 
   render (){
-    const { isLoading } = this.state;
-    return isLoading ? <Loading /> : null;
+    const { isLoading, temp } = this.state;
+    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} />;
   }
 }
