32674da0c328a3b5aa49effa5392f36a4c0852f3
JEONGSUJI <wtw9611@gmail.com>
2020-01-14 01:33:58 +0900

    statusBar, title, subtitle 및 기타 style 적용하고 마무리

--- Weather.js
+++ Weather.js
@@ -13,19 +13,27 @@ const weatherOptions = {
   },
   Thunderstorm: {
     iconName: "weather-lightning",
-    gradient: ["#373B44", "#4286f4"]
+    gradient: ["#373B44", "#4286f4"],
+    title: "Thunderstorm in the house",
+    subtitle: "Actually, outside of the house"
   },
   Drizzle: {	 
     iconName: "weather-hail",
-    gradient: ["#89F7FE", "#66A6FF"]
+    gradient: ["#89F7FE", "#66A6FF"],
+    title: "Drizzle",
+    subtitle: "Is like rain, but gay 🏳️‍🌈"
   },
   Rain:{
     iconName:"weather-rainy",
-    gradient: ["#00C6FB", "#005BEA"]
+    gradient: ["#00C6FB", "#005BEA"],
+    title: "Raining like a MF",
+    subtitle: "For more info look outside"
   },
   Snow:{
     iconName:"weather-snowy",
-    gradient: ["#7DE2FC", "#B9B6E5"]
+    gradient: ["#7DE2FC", "#B9B6E5"],
+    title: "Cold as balls",
+    subtitle: "Do you want to build a snowman? Fuck no."
   },
   Atmosphere:{
     iconName:"weather-hail",
@@ -33,15 +41,27 @@ const weatherOptions = {
   },
   Clear:{
     iconName:"weather-sunny",
-    gradient: ["#FF7300", "#FEF253"]
+    gradient: ["#FF7300", "#FEF253"],
+    title: "Sunny as fuck",
+    subtitle: "Go get your ass burnt"
   },
   Clouds:{
     iconName:"weather-cloudy",
-    gradient: ["#D7D2CC", "#304352"]
+    gradient: ["#D7D2CC", "#304352"],
+    title: "Clouds",
+    subtitle: "I know, fucking boring"
+  },
+  Mist: {
+    iconName: "weather-hail",
+    gradient: ["#4DA0B0", "#D39D38"],
+    title: "Mist!",
+    subtitle: "It's like you have no glasses on."
   },
   Dust:{
     iconName:"weather-hail",
-    gradient: ["#4DA0B0", "#D39D38"]
+    gradient: ["#4DA0B0", "#D39D38"],
+    title: "Dusty",
+    subtitle: "Thanks a lot China 🖕🏻"
   }
 }
 
@@ -55,7 +75,10 @@ export default function Weather({temp, condition}){
           <MaterialCommunityIcons size={96} name={weatherOptions[condition].iconName} color="white"/>
           <Text style={styles.temp}>{temp}˚</Text>
         </View>
-        <View style={styles.halfContainer}/>
+        <View style={{...styles.halfContainer, ...styles.textContainer}}>
+          <Text style={styles.title}>{weatherOptions[condition].title}</Text>
+          <Text style={styles.subtitle}>{weatherOptions[condition].subtitle}</Text>
+        </View>
       </LinearGradient>
   );
 }
@@ -89,5 +112,25 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems:"center"
+  },
+  title: {
+    color: "white",
+    fontSize: 44,
+    fontWeight: "300",
+    marginBottom: 10,
+    textAlign: "left"
+  },
+  subtitle: {
+    color: "white",
+    fontWeight: "600",
+    fontSize: 24,
+    textAlign: "left"
+  },
+  textContainer: {
+    paddingHorizontal: 20,
+    alignItems: "flex-start",
+    paddingHorizontal: 40,
+    justifyContent: "center",
+    flex: 1
   }
 });
